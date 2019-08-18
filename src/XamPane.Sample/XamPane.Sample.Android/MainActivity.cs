61e0a5459887ae7490eca1a76d45ba0df9f547ab
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-08-18 19:01:44 +0200

    Added initial code version

---
+++ src/XamPane.Sample/XamPane.Sample.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace XamPane.Sample.Droid
+{
+    [Activity(
+        Label = "XamPane", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ src/XamPane.Sample/XamPane.Sample/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamPane.Sample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
